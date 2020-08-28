f0dccffc4a3b9a08812a15fbfd890b8737b68062
Snugglywums <69602411+Snugglywums@users.noreply.github.com>
2020-08-28 14:54:01 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,46 +1,44 @@
-$function(){
+//This adds new list item to shopping list
+function shoppingList() {
+  
+  
+  $('#js-shopping-list-form').submit(function(event){
+    event.preventDefault();
     
-    $('#js-shopping-list-form').submit(function(event) {
-        // stop the default form submission behavior
-        event.preventDefault();
-        // in case there's already results
-        let ListItem = $('js-shopping-list-entry').val();
+    const listItem = $('.js-shopping-list-entry').val();
     
-        $('#shopping-list-entry').val(' ');
-
-
-        $('.shopping-list').append(
-          `<li>
-            <span class="shopping-item">${listItem}</span>
-            <div class="shopping-item-controls">
-              <button class="shopping-item-toggle">
-                <span class="button-label">check</span>
-              </button>
-              <button class="shopping-item-delete">
-                <span class="button-label">delete</span>
-              </button>
-            
-              </div>
-          </li>`);
-      });
-
-
-    // delete the item
-      $('.shopping-list').on('click', '.shopping-item-delete', function(event) {
-        $(this).closest('li').remove();
-      });
-
-
-    // strike through
-
-      $('.shopping-list').on('click', '.shopping-item-toggle', function(event) {
-        $(this).closest('li').find('.shopping-item').toggleClass('shopping-item__checked');
-      });
     
-    });
+   if (listItem !== "") {
+      $('.shopping-list').append(`<li>
+        <span class="shopping-item">${listItem}</span>
+        <div class="shopping-item-controls">
+          <button class="shopping-item-toggle">
+            <span class="button-label">check</span>
+          </button>
+          <button class="shopping-item-delete">
+            <span class="button-label">delete</span>
+          </button>
+        </div>
+      </li>`);
+      
+      $('.js-shopping-list-entry').val("");
+  }
+});
 
 
+//strike through
+  $("ul").on("click", ".shopping-item-toggle", function() {
+    $(this).parent().parent().find('.shopping-item').toggleClass("shopping-item__checked");
+});
 
+//delete
+$("ul").on("click", ".shopping-item-delete", function() { event.preventDefault();
+  $(event.target).closest("li").remove();
+});
+
+}
+
+$(shoppingList);
 
 
 
